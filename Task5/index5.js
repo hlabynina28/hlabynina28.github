35bb259c2c96d5f6f34d9fcafa74be166b275001
hlabynina28 <144020503+hlabynina28@users.noreply.github.com>
2023-10-01 15:35:43 +0300

    Add files via upload

--- Task5/index5.js
+++ Task5/index5.js
@@ -1,26 +1,25 @@
 function click1(event)
 {
+    console.log(event.target)
+    event.preventDefault();
     var result;
-    
     let f1=document.getElementById('field1').value;
-    let f2=document.getElementById('select1').value;
+    let f2=document.querySelector('.s').value;
     let r1=document.getElementById('result');
-    const k=/^(0|-?[1-9]\d{0,5})$/;
-    if( (!k.test(f2) ))
-    { r1.innerHTML = "Введены некорректные данные";}
+    
+   
     if (f2=="v1") f2=360;
     else if (f2=="v2") f2=500;
     else if (f2=="v3") f2= 410;
-    else f2==440;
+    else f2=440;
+    result=Number(f1)*f2;
+    r1.innerHTML =result;
     
-    { result=f1*f2;
-        r1.innerHTML ="Стоимость заказа: "+ result;
-    }
-    event.preventDefault();
+    
 }
 
 window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
-    let b=document.getElementById("button1");
+    let b=document.getElementById("button");
     b.addEventListener("click", click1);
 });
