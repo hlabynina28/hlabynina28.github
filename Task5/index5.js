5a5b149d7e353ca01a82ba27117ba4d2556a291c
hlabynina28 <144020503+hlabynina28@users.noreply.github.com>
2023-10-01 16:37:30 +0300

    Update index5.js

--- Task5/index5.js
+++ Task5/index5.js
@@ -1,19 +1,18 @@
 function click1(event) {
     event.preventDefault();
     var result;
-    let f1=document.getElementById('field1').value;
+    var f1=document.getElementById("field1").value;
+    var f2=document.querySelector(".s").value;
+    var r1=document.getElementById("result");
     f1=parseInt(f1);
-    let f2=document.querySelector('.s').value;
-    let r1=document.getElementById('result');
-    if (f2=="v1") f2=360;
-    else if (f2=="v2") f2=500;
-    else if (f2=="v3") f2= 410;
-    else f2=440;
-    result=f1*f2;
+    if (f2=="v1") result=360*f1;
+    else if (f2=="v2") result=500*f1;
+    else if (f2=="v3") result=410*f1;
+    else result=440*f1;
     r1.innerHTML =result;
 }
 
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
     let b=document.getElementById("button");
     b.addEventListener("click", click1);
